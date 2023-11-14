61f293e7979b76edaa2bc75d197eb9d1e8ef7f7f
NicholasRaynes <nicholasraynesmb@gmail.com>
2023-11-14 14:20:56 -0600

    Added sign out button + added logo to sign in screen

--- app/src/main/java/com/example/recipefinder/sign_in/SignInScreen.kt
+++ app/src/main/java/com/example/recipefinder/sign_in/SignInScreen.kt
@@ -1,17 +1,30 @@
 package com.example.recipefinder.sign_in
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.recipefinder.R
 
 @Composable
 fun SignInScreen(
@@ -35,8 +48,38 @@ fun SignInScreen(
             .padding(16.dp),
         contentAlignment = Alignment.Center
     ) {
-        Button(onClick = onSignInClick) {
-            Text(text = "Sign in")
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "RecipEase",
+                fontWeight = FontWeight(900),
+                fontFamily = FontFamily.Cursive,
+                fontSize = 60.sp,
+                modifier = Modifier.padding(
+                    top = 5.dp,
+                    start = 10.dp,
+                    bottom = 16.dp
+                )
+            )
+
+            Button(onClick = onSignInClick) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_google),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Text(
+                        text = "Sign in with Google"
+                    )
+                }
+            }
         }
     }
 }
