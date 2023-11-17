89d66e24b58b04e97d56b9d27582b1491959f024
Grey.L <berzerk10225@gmail.com>
2023-11-17 09:34:10 -0600

    Added hamburger menu w/ signout
    essentially its a hamburger menu button when clicked it opens and the signout button appears

--- app/src/main/java/com/example/recipefinder/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/recipefinder/ui/screens/HomeScreen.kt
@@ -1,12 +1,16 @@
 package com.example.recipefinder.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.example.recipefinder.sign_in.UserData
 import com.example.recipefinder.ui.components.LoadingComponent
 import com.example.recipefinder.ui.components.SuccessComponent
 import com.example.recipefinder.ui.components.ErrorComponent
+import com.example.recipefinder.ui.components.HamburgerMenuComponent
 import com.example.recipefinder.ui.viewmodel.RecipeViewIntent
 import com.example.recipefinder.ui.viewmodel.RecipeViewModel
 import com.example.recipefinder.ui.viewmodel.RecipeViewState
@@ -18,14 +22,23 @@ fun HomeScreen(
     onSignOut: () -> Unit
 ) {
     val state by recipeViewModel.state
+    val expandedState = remember { mutableStateOf(false) }
 
-    when(state) {
+    when (state) {
         is RecipeViewState.Loading -> LoadingComponent()
         is RecipeViewState.Success -> {
             val recipes = (state as RecipeViewState.Success).recipes
-            SuccessComponent(recipes = recipes, onSearchClicked = {query ->
-                recipeViewModel.processIntent(RecipeViewIntent.SearchRecipes(query))
-            }, userData = userData, onSignOut = onSignOut)
+            Column {
+                HamburgerMenuComponent(expandedState, userData, onSignOut)
+                SuccessComponent(
+                    recipes = recipes,
+                    onSearchClicked = { query ->
+                        recipeViewModel.processIntent(RecipeViewIntent.SearchRecipes(query))
+                    },
+                    userData = userData,
+                    onSignOut = onSignOut
+                )
+            }
         }
         is RecipeViewState.Error -> {
             val message = (state as RecipeViewState.Error).message
